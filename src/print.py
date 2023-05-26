466c3519a7e9926204326e7dcb3f5af8e5f8c6ab
ThiagoAlmarazz21 <115847063+ThiagoAlmarazz21@users.noreply.github.com>
2023-05-26 10:52:33 -0300

    print en python

---
+++ src/print.py
@@ -0,0 +1,12 @@
+# ESTOS SON MIS PRIMEROS PASOS EN PYTHON
+
+print('Hola, esto es Python!!')
+print('Mi nombre es Thiago, y tengo 19 años')
+
+# ALGUNAS OPERACIONES 
+print(10 + 9)
+print(10 - 5)
+print(10 * 2)
+print(20 / 2)
+
+# ESTO ES UN COMENTARIO <---------

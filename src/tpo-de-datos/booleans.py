c7ef4d14fc65121bdc1758be386b0074d309848e
ThiagoAlmarazz21 <115847063+ThiagoAlmarazz21@users.noreply.github.com>
2023-05-26 12:45:07 -0300

    todo sobre booleanos

--- src/tpo-de-datos/booleans.py
+++ src/tpo-de-datos/booleans.py
@@ -1,3 +1,12 @@
 print('Este es el archivo de Booleans!!')
 
 is_single = True
+print('tipo de is_single =>', type(is_single))
+is_single = False
+print('cambio de valor en is_single =>', is_single)
+
+print(not True)
+print(not False)
+
+is_single = not is_single
+print(is_single)

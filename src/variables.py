3b5abc70314189569ce773972ef78ef9d5ea23f6
ThiagoAlmarazz21 <115847063+ThiagoAlmarazz21@users.noreply.github.com>
2023-05-26 11:12:03 -0300

    todo sobre variables

---
+++ src/variables.py
@@ -0,0 +1,16 @@
+print('Este es el archivo de variables!')
+
+# ESTO ES UNA VARIABLE 
+my_name = 'Thiago'
+print('Mi nombre es:', my_name)
+
+my_age = 19
+print('Mi edad es:', my_age)
+# CAMBIANDO VALOR DE VARIABLE 
+my_age = 20
+print('Mi edad cambio a:', my_age)
+
+# INPUT
+name = input('¿Cuál es tu nombre?')
+print(f'Hola {name}, bienvenido al mundo python!')
+

ba950adbe6b7a968c3b11356afd8bd2b1c960850
ThiagoAlmarazz21 <115847063+ThiagoAlmarazz21@users.noreply.github.com>
2023-05-26 13:21:13 -0300

    operadores aritmeticos

---
+++ src/operador-aritmético.py
@@ -0,0 +1,14 @@
+print('Este es el archivo de Operador Aritmético!!')
+
+print('Suma',10 + 10)
+print('Resta',10 - 5)
+print('Multiplicacion',10 * 2)
+print('Division',20 / 2)
+print('Modulo',10 % 2)
+# solo nos quedamos con el entero 👇
+print('Quedarnos con el numero entero',10 // 3)
+print('Exponencial',2 ** 3)
+print('Jerarquia', 2 ** 3 + 3 - 7 / 1 // 4)
+
+print('Hola' + ' mundo')
+print('Hola ' * 3)
---
+++ src/trasnformation.py
@@ -0,0 +1,20 @@
+print('Este es el archivo de Transformacion de Tipos!!')
+
+name = 'Thiago'
+print(type(name))
+name = 12
+print(type(name))
+name = False
+print(type(name))
+
+print('Thiago' + ' Almaraz')
+print(12 + 2)
+print('Nicolas' + ' 1')
+
+age = 20
+print(f'Mi edad es {age}')
+
+age = int(input('Escribe tu edad: '))
+print(type(age))
+print(f'Tu edad es {age}')
+print(f'Tu edad en 10 años será {age + 10}')

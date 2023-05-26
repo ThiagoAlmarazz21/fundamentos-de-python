fde5ece753113768ca64fdfc0eebf80e1c2726c5
ThiagoAlmarazz21 <115847063+ThiagoAlmarazz21@users.noreply.github.com>
2023-05-26 11:48:42 -0300

    todo sobre tipo de datos

---
+++ src/tipo-de-datos.py
@@ -0,0 +1,22 @@
+print('Este es el archivo de Tipos de Datos!')
+
+# STRING (str) esto es una cadena de texto
+my_name = 'Thiago'
+print('my_name =>', my_name)
+print(type(my_name))
+
+# ENTEROS O NUMEROS (int)
+my_age = 19
+print('my_age =>', my_age)
+print(type(my_age))
+
+# BOOLEANS (bool)
+# solo se le puede asignar True or False en datos booleanos
+is_sinlge = True
+print('is_single =>', is_sinlge)
+print(type(is_sinlge))
+
+# INPUT
+age = input('Cual es tu edad? ')
+tipo = type(age)
+print(f"Tu edad es '{age}', y es de tipo {tipo}")

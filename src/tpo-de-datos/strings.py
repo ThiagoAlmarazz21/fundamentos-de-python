418f0ed14af31a7d8cec7dd1262e9df3d9c5dec8
ThiagoAlmarazz21 <115847063+ThiagoAlmarazz21@users.noreply.github.com>
2023-05-26 11:56:53 -0300

    numbers

---
+++ src/tpo-de-datos/strings.py
@@ -0,0 +1,8 @@
+print('Este es el archivo de strings!!')
+
+name = input('Cual es tu nombre? ')
+last_name= input('Cual es tu apellido? ')
+age = input('Cuantos años tienes? ')
+
+frase =  f'Hola {name} {last_name}, tu edad es {age}'
+print(frase)

3c300163ef3f3758a19bc650c4dfddb9720e2675
ThiagoAlmarazz21 <115847063+ThiagoAlmarazz21@users.noreply.github.com>
2023-05-29 11:29:56 -0300

    condicional if

---
+++ src/condicional-if.py
@@ -0,0 +1,33 @@
+print('Este es el archivo del condicional "if"')
+
+if True:
+  print('Deberia ejecutarse')
+
+if False: 
+  print('Nunca se ejecuta')
+
+
+pet = input('¿Cual es tu mascota favorita? ')
+
+if pet == 'perro':
+  print('Genial, tienes buen gusto!')
+elif pet == 'gato': 
+  print('Espero tengas suerte')
+else:
+  print('Debes ingresar "perro" o "gato"')
+
+
+stock = int(input('Ingrese un número de stock: '))
+
+if stock >= 100 and stock <= 1000:
+  print(f'El stock es correcto, su valor: "{stock}"')
+else: 
+  print('Por favor ingrese un valor entre 100 y 1000')
+
+num = int(input('Ingrese un valor para saber si es par o impar: '))
+
+# PROGRAMA PARA SABER SI UN NÚMERO ES PAR O IMPAR
+if num % 2 == 0:
+  print('El número es PAR')
+else: 
+  print('El número es IMPAR')

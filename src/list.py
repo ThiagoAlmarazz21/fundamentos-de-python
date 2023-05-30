2e268a2f27d3a0020a5bafcad59bfd7d0d0a1b93
ThiagoAlmarazz21 <115847063+ThiagoAlmarazz21@users.noreply.github.com>
2023-05-30 11:23:01 -0300

    todo sobre listas

---
+++ src/list.py
@@ -0,0 +1,24 @@
+print('Este es el archivo de List!!!')
+
+numbers = [1,2,3,4,5]
+print(numbers)
+print(type(numbers))
+
+tasks = ['lavar los platos', 'limpiar la cocina']
+print(tasks)
+
+types = [1, True, 'hola']
+print(types)
+
+print(numbers[0])
+print(tasks[0])
+
+text = 'Hola'
+# text[0] = 'W'
+
+tasks[0] = 'lavarme los dientes'
+print(tasks)
+
+print(numbers[:3])
+print(True in types)
+print('hola' in types)
